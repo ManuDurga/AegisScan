c261ca86e69d07e1c9f20481ea3b858cd1bfce40
ManuDurga <you@example.com>
2026-07-27 03:10:55 +0530

    parse ports empty input updated

--- PortScanner.py
+++ PortScanner.py
@@ -21,24 +21,25 @@ def scan_ports(target_ip,ports,timeout=1.2):
     return open_ports
 
 def parse_ports(ports_input):
-    ports=set()
-    port_tokens=ports_input.split(',')#converting "1,2,3" to "1","2","3" and "4-7" to "4","5","6","7"
-    for i in port_tokens:
-        if '-' in i:
-            st_end = i.split('-')
-            if(len(st_end)==2 and st_end[0].isdigit() and st_end[1].isdigit()):
-                st=int(st_end[0])
-                end=int(st_end[1])
-                if st > end:
-                        raise ValueError("Invalid port range")
-                ports.update(range(st,end+1))
+    if len(ports_input)!=0:
+        ports=set()
+        port_tokens=ports_input.split(',')#converting "1,2,3" to "1","2","3" and "4-7" to "4","5","6","7"
+        for i in port_tokens:
+            if '-' in i:
+                st_end = i.split('-')
+                if(len(st_end)==2 and st_end[0].isdigit() and st_end[1].isdigit()):
+                    st=int(st_end[0])
+                    end=int(st_end[1])
+                    if st > end:
+                            raise ValueError("Invalid port range")
+                    ports.update(range(st,end+1))
+                else:
+                    raise ValueError("Invalid port format")
             else:
-                raise ValueError("Invalid port format")
-        else:
-            if i.isdigit() and int(i)<65536:
-                ports.add(int(i))
-            else:
-                raise ValueError("Invalid port value")
+                if i.isdigit() and int(i)<65536:
+                    ports.add(int(i))
+                else:
+                    raise ValueError("Invalid port value")
     return sorted(ports)
 
 scan_ports(target,ports)
